de9f2fe34ea2c91d5b92c37843741167238b1ade
Sam Philip <seivadmas@gmail.com>
2014-04-03 18:09:54 -0300

    Configured MANDRILL as mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,16 @@ LearnRails::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mandrillapp.com",
+    port: 25,
+    user_name: ENV["MANDRILL_USERNAME"],
+    password: ENV["MANDRILL_API_KEY"]
+  }
+
+  # ActionMailer config
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
 end
+
