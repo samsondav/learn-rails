d7cd1c7464057f4ff526b5638c4023f806960850
Sam Philip <seivadmas@gmail.com>
2014-04-04 20:44:19 -0300

    Prettied up layout using rails_layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,7 @@
 class VisitorsController < ApplicationController
   def new
-    Rails.logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
-    Rails.logger.debug "DEBUG: Owner name is: #{@owner.name}"
-    render 'visitors/new' # code redundant, this is the default action anyway
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My birthday is soon.'
   end
 end
