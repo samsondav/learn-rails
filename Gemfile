c49a074645c237c72f78ea626c6136086bf444ea
Sam Philip <seivadmas@gmail.com>
2014-04-03 21:50:50 -0300

    Added loggers to Visitors controller and Owner model

--- Gemfile
+++ Gemfile
@@ -28,4 +28,5 @@ group :development do
   gem 'better_errors'
   gem 'quiet_assets'
   gem 'rails_layout'
+  gem 'pry'
 end
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,8 @@
 class VisitorsController < ApplicationController
   def new
+    Rails.logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
+    Rails.logger.debug "DEBUG: Owner name is: #{@owner.name}"
     render 'visitors/new' # code redundant, this is the default action anyway
   end
 end
