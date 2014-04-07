4a5e8185010f3cbfa02b68970252665b889ed847
Sam Philip <seivadmas@gmail.com>
2014-04-07 10:52:10 -0300

    Assets compile for Heroku

--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -1,5 +1,6 @@
-Analytics = AnalyticsRuby #alias for convenience
-Analytics.init({
-  secret: '3p0rm001b7',
-  on_error: Proc.new {|status, msg| print msg}
-})
+## commented out due to analytics_ruby gem only being loaded in the development environment
+# Analytics = AnalyticsRuby #alias for convenience
+# Analytics.init({
+#   secret: '3p0rm001b7',
+#   on_error: Proc.new {|status, msg| print msg}
+# })
